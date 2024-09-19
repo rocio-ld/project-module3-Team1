891edae1f59308ed717c23682856d642d4279833
Shannnon Evora Lopes Rodrigues <shannonelr9208@icloud.com>
2024-09-19 21:46:29 +0200

    change final demo

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,13 +1,20 @@
 import {Link} from 'react-router-dom';
+import '../scss/Landing.scss';
+
 
 
 function Landing() {
   return (
-    <main>
-      <h3>PROYECTOS MOLONES</h3>
-      <Link to='/create'>crear proyecto</Link>
+    <main className="container" >
+      <h2 className="title" >Proyectos Molones</h2>
+      <p className="container__text">Escaparate en línea para recoger ideas a través de la tecnología</p>
+      <Link className="create" to='/create'>Nuevo proyecto</Link> 
+      <div> 
+      </div>  
     </main>
+    
+    
   )
 }
 
-export default Landing
+export default Landing;
--- src/components/SectionProject.jsx
+++ src/components/SectionProject.jsx
@@ -4,7 +4,7 @@ import '../scss/SectionProject.scss';
 function SectionProject (){
     return(
       <section className="hero">
-        <h2 className="title">Proyectos molones</h2>
+        <h2 className="title">Proyectos Molones</h2>
         <p className="hero__text">Escaparate en línea para recoger ideas a través de la tecnología</p>
         <a className="button--link" href="./">Ver proyectos</a>
       </section>
